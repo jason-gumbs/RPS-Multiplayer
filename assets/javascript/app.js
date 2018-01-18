3a440c2eb86eedb28d27f363511607dd39f42804
Will <fwgumbs@gmail.com>
2018-01-17 20:11:05 -0500

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+
+$(".top_image1 ,.top_image2 , .choice2 , .choice").hide();
+
 function blink_text() {
     $('.blink').fadeOut(500);
     $('.blink').fadeIn(500);
@@ -15,38 +18,67 @@ setInterval(blink_text, 1000);
   };
   firebase.initializeApp(config);
    var database = firebase.database();
-   var player = 0;
+   var player1 = "";
+    var player2 = "";
    var key = 0;
     var x = 1;
     var name = "";
-   
+    var name2 = "";
+
+   //recieve and inputs player 1 into firebase database
 $("#button").on("click", function(event) {
       // Prevent the page from refreshing
       event.preventDefault();
-
-      // Get inputs
+ // Get inputs
       name = $("#namer").val().trim();
       $("#user").text(name);
+      $(".top_image1 , .choice").show();
       $(".form").hide();
         key++;
-        player++;
+       
          
       if (key === 3){
          	 database.ref().remove();
          	}
          	else{
-         		
-
-         // Change what is saved in firebase
-      database.ref('players/name').push({
-        name: name,
-        player: player,
-        key: key,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-        
+ 		// Change what is saved in firebase
+   
+      database.ref('players/name').push ({
+				
+				player1: name,
+				key: key,
+				dateAdded: firebase.database.ServerValue.TIMESTAMP
+			}) 
+		
       
+      
+      }});
+   //recieve and inputs player 2 into firebase database
 
-      })
+$("#button2").on("click", function(event) {
+      // Prevent the page from refreshing
+      event.preventDefault();
+ // Get inputs
+      name2 = $("#namer2").val().trim();
+      $("#user2").text(name);
+      $(".top_image2 , .choice2 ").show();
+      $(".form2").hide();
+        key++;
+       
+         
+      if (key === 3){
+         	 database.ref().remove();
+         	}
+         	else{
+ 		// Change what is saved in firebase
+   
+      database.ref('players/name').push ({
+				
+				player2: name2,
+				key: key,
+				dateAdded: firebase.database.ServerValue.TIMESTAMP
+			}) 
+		
       
       
       }});
@@ -54,17 +86,44 @@ $("#button").on("click", function(event) {
 
 
 
+
       
     
-//display your name
-     database.ref('players/name').orderByChild("dateAdded").limitToLast(1).on("child_added", function(childSnapshot) {
+//display player 1
+     database.ref('players/name').on("child_added", function(childSnapshot) {
       // full list of items to the well
-      $("#user2").text(childSnapshot.val().name);
-      player = childSnapshot.val().player;
+      $("#user").text(childSnapshot.val().player1);
+      player1 = childSnapshot.val().player1;
       key = childSnapshot.val().key;
+      if(player1){
+      	 $(".form").hide();
+
+      }
        
-      console.log(player);
+      
       console.log(key);
+
+
+    // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+
+     //display player 2
+     database.ref('players/name').on("child_added", function(childSnapshot) {
+      // full list of items to the well
+      $("#user2").text(childSnapshot.val().player2);
+       player2 = childSnapshot.val().player2;
+     
+      key = childSnapshot.val().key;
+      if(player2){
+      	 $(".form2").hide();
+
+      }
+       
+    
+      console.log(key);
+      console.log(player2);
 
 
     // Handle the errors
--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,9 +1,10 @@
 var wins = 0;
 var losses = 0;
 var ties = 0;
-var userGuess;
+var userGuess ="";
 var computerGuess; 
   var key1 = 1;
+
 
 
 $(".choice").on("click", function(event) {
@@ -11,26 +12,69 @@ $(".choice").on("click", function(event) {
       event.preventDefault();
 		// Get inputs
        userGuess = $(this).attr('value');
-       database.ref('players/name').update({
+       database.ref('players/name/player1').push({
         userGuess: userGuess,
         //wins: wins,
         //losses: losses,
         //ties: ties,
         //computerGuess: computerGuess
-  });
-        database.ref('players/name').orderByChild("name").limitToLast(1).on("child_added", function(childSnapshot) {
+  }); });
+
+//-----------------------------------------------------------------///////
+        database.ref('players/name/player1').on("child_added", function(childSnapshot) {
       // full list of items to the well
       userGuess = childSnapshot.val().userGuess;
-      $("#user2").text(userGuess);
+      $("#user").append(userGuess);
    		console.log(userGuess);
      
 // Handle the errors
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
     });
+
+   //-------------------------------------------------------------------//
+   //-----------------------------------------------------------------//
+        $(".choice2").on("click", function(event) {
+      // Prevent the page from refreshing
+      event.preventDefault();
+		// Get inputs
+       computerGuess = $(this).attr('value');
+       database.ref('players/name/player2').push({
+        computerGuess: computerGuess,
+        //wins: wins,
+        //losses: losses,
+        //ties: ties,
+        //computerGuess: computerGuess
+  });
+
+        database.ref('players/name/player2').on("child_added", function(childSnapshot) {
+      // full list of items to the well
+      computerGuess = childSnapshot.val().computerGuess;
+      $("#user2").text(computerGuess);
+   		console.log(computerGuess);
+     
+// Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+     
      //database.ref().remove();
-       //computerGuess = ;
-      console.log(userGuess);
+      //computerGuess = ;
+
+        database.ref('players/name').on("child_added", function(childSnapshot) {
+      // full list of items to the well
+      computerGuess = childSnapshot.val().computerGuess;
+      
+   		console.log(computerGuess);
+   		console.log(userGuess);
+
+     
+// Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+      console.log("userGuess");
+
 
        if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
         if ((userGuess === "r") && (computerGuess === "s")) {
@@ -47,5 +91,5 @@ $(".choice").on("click", function(event) {
           losses++;
         } else if (userGuess === computerGuess) {
           ties++;
-        }}
-      });
+        }}});
+      
